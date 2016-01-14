9b7ba6605dd2ff45c61114367e9d7d3fb408a59b
jackson cafazzo and Pablo Vera <pablovera.pdx@gmail.com>
2016-01-13 16:42:55 -0800

    add a ul list for our arrey

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
   $("h1").click(function() {
-    debugger;
     var iceCreams = ["strawberry", "pistacio", "peanut-butter chocolate", "vanilla", "greek yogurt"];
     for (var index = 0; index < iceCreams.length; index +=1) {
-      alert("I love " + iceCreams[index] + "!");
+      $("ul").append("<li>I love " + iceCreams[index] + "!</li>");
     }
     // iceCreams.forEach(function(iceCream) {
       // alert(iceCream);
@@ -11,7 +10,4 @@ $(document).ready(function() {
   });
 });
 
-    // iceCreams.forEach(function(iceCream) {
-
-
-    // event.preventDefault();
+    //event.preventDefault();
