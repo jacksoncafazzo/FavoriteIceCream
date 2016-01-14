efa2eec466677b8234fdfff557a400928ba1efcb
jackson cafazzo and Pablo Vera <pablovera.pdx@gmail.com>
2016-01-13 16:18:08 -0800

    add initial files, defined array, and make alerts

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    debugger;
+    var iceCreams = ["strawberry", "pistacio", "peanut-butter chocolate", "vanilla", "greek yogurt"];
+    for (var index = 0; index < iceCreams.length; index +=1) {
+      alert("I love " + iceCreams[index] + "!");
+    }
+    // iceCreams.forEach(function(iceCream) {
+      // $("li").text(iceCream);
+      // alert(iceCream);
+    // });
+  });
+});
+
+    // iceCreams.forEach(function(iceCream) {
+
+
+    // event.preventDefault();
